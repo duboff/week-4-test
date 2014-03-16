0af1d8c89ce258bc0adfb116cae5de1d9859e39d
Mikhail Dubov <mdubov@gmail.com>
2014-03-16 23:04:59 +0000

    Takeaway class now send text messages. Seems to work fine.

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,4 +1,5 @@
 class Takeaway
+  
   attr_reader :price_list
 
   def initialize(price_list)
@@ -6,9 +7,27 @@ class Takeaway
   end
 
   def place_order(quantities, total)
-    sum = quantities.keys.inject(0) {|result, key| result + price_list[key] * quantities[key] }
+    raise 'Quantities should be passed on as a hash' if quantities.class != Hash
     raise 'The total appears to be wrong' if total != sum
+    sum = quantities.keys.inject(0) {|result, key| result + price_list[key] * quantities[key] }
+    time = time_in_1_hour
+    text = "Thank you for your order. We will deliver it before #{time.hour}:#{time.min}."
+    send_text(text)
   end
 
+  def time_in_1_hour
+    Time.now + (60 * 60)
+  end
+
+  def send_text(text)
+    account_sid = 'AC965151dcbe7db4009698c52c423e1d60'
+    auth_token = 'e2d56b7f3fe8edf7c99fc766377c001b'
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    @client.account.messages.create(
+      :from => '+441792720314',
+      :to => '+447595666228',
+      :body => text
+    )
+  end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,9 +1,11 @@
 require 'takeaway'
+require 'twilio-ruby'
 
 describe Takeaway do
   
   let (:dish_list) {{'apple' => 2, 'steak' => 20, 'salad' => 10, 'burger' => 15}}
   let (:takeaway) {Takeaway.new(dish_list)}
+  # let (:ta_double) {double :takeaway}
   
   it 'has a list of dishes on initialization' do
     expect(takeaway.price_list.class).to eq Hash
@@ -18,8 +20,22 @@ describe Takeaway do
     expect(lambda {takeaway.place_order({'burger' => 2, 'salad' => 1}, 5)}).to raise_error(RuntimeError)
   end
   it 'no error when total is right' do
+    takeaway.stub("send_text")
     expect(lambda {takeaway.place_order({'burger' => 2, 'salad' => 1}, 40)}).not_to raise_error
   end
+
+  it 'knows time in 1 hour' do
+    time = Time.now + (60 * 60)
+    expect(takeaway.time_in_1_hour.to_i).to eq time.to_i
+  end
+
+  it 'sends a text when order is placed' do
+    takeaway.stub("send_text")
+    expect(takeaway).to receive(:send_text)
+    takeaway.place_order({'burger' => 2, 'salad' => 1}, 40)
+  end
+
+
 
 
 end
